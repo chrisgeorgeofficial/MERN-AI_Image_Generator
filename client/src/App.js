a0fcdd7d83dd0ad169a4e7ff97aa2561114e85de
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2024-06-29 15:01:10 -0700

    index and App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
 `;
 
 const Wrapper = styled.div`
